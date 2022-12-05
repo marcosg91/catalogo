6557989c2cecfea0dd94c8a4243601f66d2e7d4b
unknown <marcos_g91@hotmail.com>
2022-12-05 04:20:14 -0300

    templates modificado

--- catalogo/catalogo/views.py
+++ catalogo/catalogo/views.py
@@ -1,8 +1,8 @@
-#dentro de la libreria shortcuts esta la funcion render 
 from django.shortcuts import render 
 
 def inicio(request):
     template_name = "index.html"
     contexto = {
+        'nombre': "Marcos"
     }
     return render( request, template_name, contexto)
