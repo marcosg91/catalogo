f0fcad5c044f56408ded9127611e486ea99d69fd
unknown <marcos_g91@hotmail.com>
2022-12-17 18:31:17 -0300

    paginado de listado de productos

--- catalogo/products/views.py
+++ catalogo/products/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render 
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.urls import reverse 
 
 from .forms import ProductoForm
 from .models import Producto
-def admin_listado_productos(request):
+"""def admin_listado_productos(request):
     template_name='productos/listado.html'
 
     contexto = {
         'productos': Producto.objects.all()
     }
-    return render( request, template_name, contexto)
+    return render( request, template_name, contexto)"""
 
 # vista que nos va a permitir crear algo 
+class AdminListadoProductos (ListView):
+    template_name= "productos/listado.html"
+    model= Producto
+    context_object_name= "productos" 
+    paginate_by= 2
+
 class NuevoProducto(CreateView):
     model= Producto
     template_name= "productos/nuevo_producto.html"
