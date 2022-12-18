db4ae9b96174940f80f96764e573fd6138ff4f92
unknown <marcos_g91@hotmail.com>
2022-12-18 03:14:26 -0300

    carpeta media

--- catalogo/catalogo/urls.py
+++ catalogo/catalogo/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from django.contrib.auth import views as auth_views
 
 
@@ -14,5 +17,5 @@ urlpatterns = [
     #Includes 
     path('productos/', include('products.urls')),
     path('usuarios/', include('usuarios.urls'))
-]
+] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
    
--- catalogo/products/models.py
+++ catalogo/products/models.py
@@ -10,15 +10,26 @@ class Producto(models.Model):
     activo = models.BooleanField(default=True)
 
     # relacion para un producto una categoria 
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, related_name="categorias") # CASCADE borra en cascada todos los elementos si se elimina una categoria
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, related_name="categorias", blank=True) # CASCADE borra en cascada todos los elementos si se elimina una categoria
 
     # relacion para un producto mas de una categoria 
     categoria2 = models.ManyToManyField(Categoria) # aqui django crea en la base de datos automaticamente una tabla intermedia y relaciona las fk de productos y categorias 
+
+    imagen = models.ImageField(upload_to="productos", null=True, blank=True)
+    # models.FieldFile campo para archivos 
+    # models.BinaryField campo para guardar ruta mas archivo completo
+
 
    # ficha = models.OneToOneField(Ficha) en el caso de requerir que un producto tenga una sola ficha y esa ficha tenga un solo producto
 
     def __str__(self):
         return self.nombre
+
+"""
+x = Producto.objects.filter(id = 1).first() #exists()
+if x:
+    pass
+"""
 
 """
 c = Categoria.objects.get(id=1)
